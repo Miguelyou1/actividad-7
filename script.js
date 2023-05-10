388fe1ba878c283fdc23948a775b1a531814fcf6
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-10 19:11:31 +0200

    script

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const numeroAleatorio = Math.floor(Math.random() * 15) + 1;
 // Definir número de intentos disponibles
 const intentosMaximos = 3;
 let intentosRestantes = intentosMaximos;
+
+// Función para comprobar si el número es correcto
+function comprobarNumero(numero) {
+    if (numero === numeroAleatorio) {
+      console.log("¡Felicidades! Has acertado el número.");
+      return true;
