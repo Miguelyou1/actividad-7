53c934f205ff8d2893f514e9d254ccdb4a5f8a19
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-10 19:10:08 +0200

    script

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // Generar número aleatorio entre 1 y 15
 const numeroAleatorio = Math.floor(Math.random() * 15) + 1;
+
+// Definir número de intentos disponibles
+const intentosMaximos = 3;
+let intentosRestantes = intentosMaximos;
