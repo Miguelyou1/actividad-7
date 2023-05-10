747b78a54bbed159c4e4a181295150a122e385eb
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-10 19:08:58 +0200

    script

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-const numeroAdivinar = Math.floor(Math.random() * 15) + 1;
-let intentos = 3;
-function pedirNumero() {
-    const numero = prompt('Introduzca un numero entre 1 y 15, tienes 3 intentos');
-    return parseInt(numero);
- }
-function comprobarNumero(numero) {
-    intentos+;
-    if (numero === numeroAdivinar) {
-        alert(`Has acertado el numero en ${intentos} intentos.`);
-    return true;
- } 
-else if (numero < numeroAdivinar) {
-    alert('El numero es menor.');
- }
-else {
-    alert('El numero es mayor.');
- }
-if (intentos === 3 && numero != numeroAdivinar) {
-    alert(`Se acabaron tus intentos, el numero era ${ numeroAdivinar }.`);
-  }
-
+// Generar número aleatorio entre 1 y 15
+const numeroAleatorio = Math.floor(Math.random() * 15) + 1;
