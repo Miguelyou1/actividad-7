25e2e74539e01cced23e38abac9118a90aea7236
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-10 13:21:07 +0200

    script

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ let intentos = 0;
 function pedirNumero() {
     const numero = prompt('Introduzca un numero entre 1 y 15, tienes 3 intentos');
     return parseInt(numero);
-}
+ }
 function comprobarNumero(numero) {
     intentos+;
     if (numero === numeroAdivinar) {
         alert(`Has acertado el numero en ${intentos} intentos.`);
     return true;
-} 
+ } 
 else if (numero < numeroAdivinar) {
     alert('El numero es menor.');
-}
+ }
 else {
     alert('El numero es mayor.');
+ }
+if (intentos === 3 && numero != numeroAdivinar) {
+    alert(`Se acabaron tus intentos, el numero era ${ numeroAdivinar }.`);
   }
+  
