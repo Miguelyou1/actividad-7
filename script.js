7734ae9bf6d7e249b5e49736f96c11b47b561ca6
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-10 19:29:38 +0200

    script

--- script.js
+++ script.js
@@ -7,23 +7,25 @@ let intentosRestantes = intentosMaximos;
 
 // Función para comprobar si el número es correcto
 function comprobarNumero(numero) {
-    if (numero === numeroAleatorio) {
-        console.log("¡Felicidades! Has acertado el número.");
-        return true;
-    } else {
-        intentosRestantes--;
-        console.log(`Lo siento, el número es ${numero < numeroAleatorio ? "mayor" : "menor"} al que has introducido. Te quedan ${intentosRestantes} intentos.`);
-        return false;
-    }
+  if (numero === numeroAleatorio) {
+    console.log("¡Felicidades! Has acertado el número.");
+    return true;
+  } else {
+    intentosRestantes--;
+    console.log(`Lo siento, el número es ${numero < numeroAleatorio ? "mayor" : "menor"} al que has introducido. Te quedan ${intentosRestantes} intentos.`);
+    return false;
+  }
 }
+
 // Pedir al usuario que adivine el número hasta que acierte o se acaben los intentos
 while (intentosRestantes > 0) {
-    const numeroUsuario = parseInt(prompt(`Introduce un número entre 1 y 15. Te quedan ${intentosRestantes} intentos.`));
-    if (comprobarNumero(numeroUsuario)) {
-        break;
-    }
+  const numeroUsuario = parseInt(prompt(`Introduce un número entre 1 y 15. Te quedan ${intentosRestantes} intentos.`));
+  if (comprobarNumero(numeroUsuario)) {
+    break;
+  }
 }
+
 // Si se han agotado los intentos, informar al usuario de que ha perdido
 if (intentosRestantes === 0) {
-    console.log(`Lo siento, has perdido. El número era ${numeroAleatorio}.`);
+  console.log(`Lo siento, has perdido. El número era ${numeroAleatorio}.`);
 }
