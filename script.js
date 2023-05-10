62e6f97a6198a7b7005c3ad6605c7876a188ed17
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-10 13:01:19 +0200

    script

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const numeroAdivinar = Math.floor(Math.random() * 15) + 1;
 let intentos = 0;
 function pedirNumero() {
-    const numero = prompt('Introduzca un número entre 1 y 15, tienes 3 intentos');
+    const numero = prompt('Introduzca un numero entre 1 y 15, tienes 3 intentos');
     return parseInt(numero);
 }
+function comprobarNumero(numero) {
+    intentos+;
