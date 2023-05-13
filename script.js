61e92ae8974031e4e74c28e15aaf12cb7398b322
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-13 12:17:21 +0200

    script

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
-// Generar un número aleatorio entre 1 y 15
-const numeroAleatorio = Math.floor(Math.random() * 15) + 1;
+let intentos = 0;
+const numeroAdivinar = Math.floor(Math.random() * 15) + 1;
 
-// Establecer el número máximo de intentos y el número actual de intentos
-const numIntentosMaximo = 3;
-let numIntentosActual = 0;
+for (let i = 0; i < 3; i++) {
+  const numero = parseInt(prompt(`Tienes que adivinar el número que estoy pensando del 1 al 15, tienes ${3 - intentos} intentos`));
+  intentos++;
 
-// Función para verificar si el usuario ha adivinado el número
-function verificarAdivinanza(numeroAdivinanza) {
-  numIntentosActual++;
-  var men
-  
-  if (numeroAdivinanza === numeroAleatorio) {
-    men = console.log("¡Felicidades! Has adivinado el número.");
-    return true;
-  } else if (numIntentosActual === numIntentosMaximo) {
-    men = console.log("Lo siento, has perdido. El número era " + numeroAleatorio + ".");
-    return false;
-  } else {
-    const pista = numeroAdivinanza > numeroAleatorio ? "menor" : "mayor";
-    men = console.log("Incorrecto. El número que buscas es " + pista + " al que has introducido. Te quedan " + (numIntentosMaximo - numIntentosActual) + " intentos.");
-    return false;
-  }
-}
-
-// Pedir al usuario que adivine el número
-console.log("Bienvenido al juego. Adivina un número entre 1 y 15. Tienes " + numIntentosMaximo + " intentos.");
-
-while (numIntentosActual < numIntentosMaximo) {
-  const adivinanza = parseInt(prompt("Intento #" + (numIntentosActual + 1) + ":"));
-  
-  if (verificarAdivinanza(adivinanza)) {
+  if (numero === numeroAdivinar) {
+    alert(`¡Muy bien, solo has necesitado ${intentos} intentos.`);
     break;
+  } else if (numero < numeroAdivinar) {
+    alert('El número que has introducido es menor al que estoy pensando.');
+  } else {
+    alert('El número que has introducido es mayor al que estoy pensando.');
+  }
+
+  if (intentos === 3 && numero != numeroAdivinar) {
+    alert(`Lo siento, no has adivinado el número, el numero era ${numeroAdivinar}.`);
   }
 }
+
 
